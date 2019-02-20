ce6d1a4e265ecbe64a0d4b53e56b94a82c355501
doohong <wnghd94@gmail.com>
2019-02-20 17:41:30 +0900

    thymeleaf

--- src/main/java/com/doohong/board/controller/HelloController.java
+++ src/main/java/com/doohong/board/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.doohong.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
-    public  String index(){
+    public  String index(String name, Model model){
+        model.addAttribute("name",name);
         return "index";
     }
 }
