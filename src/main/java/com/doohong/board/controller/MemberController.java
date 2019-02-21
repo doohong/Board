38f628d21ed3ece8ae9da013c7b81b7338f0ae65
doohong <wnghd94@gmail.com>
2019-02-21 17:47:22 +0900

    add domain Member Class

--- src/main/java/com/doohong/board/controller/MemberController.java
+++ src/main/java/com/doohong/board/controller/MemberController.java
@@ -1,17 +1,36 @@
 package com.doohong.board.controller;
 
+import com.doohong.board.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
+    List<Member> memberList = new ArrayList<>();
     //member
     @GetMapping
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("list",memberList);
         return "member";
+
+
     }
 
+    @PostMapping
+    public String create(Member member, Model model){
+        log.info("member {} ",member);
+        memberList.add(member);
+        return "redirect:/member";
+    }
 }
---
+++ src/main/java/com/doohong/board/domain/Member.java
@@ -0,0 +1,9 @@
+package com.doohong.board.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String username;
+    private String password;
+}
