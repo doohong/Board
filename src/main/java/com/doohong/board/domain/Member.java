4e03a5354167a3cce2f867cb5b819a32d1986ad2
doohong <wnghd94@gmail.com>
2019-02-22 16:12:27 +0900

    add validation

--- src/main/java/com/doohong/board/domain/Member.java
+++ src/main/java/com/doohong/board/domain/Member.java
@@ -1,9 +1,20 @@
 package com.doohong.board.domain;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class Member {
+
+    @Length(min=2)
+    @Length(max=20)
+    @Email
+    @NotBlank
     private String username;
+
+    @Length(min=2)
     private String password;
 }
